92fbef96494a2db4e9453183ba555abce1b8747f
kleyson Castro <kleysoncastro99@gmail.com>
2023-01-11 14:38:56 -0300

    chore: add component transaction

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from "../Summary";
+import { Transaction } from "../TransactonsTable";
 import { Container } from "./styles";
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <Transaction />
         </Container>
     );
 }
---
+++ src/components/TransactonsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function Transaction() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Componetes</td>
+                        <td className="retirada">-R$2.500.00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/06/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Pojeto Oria</td>
+                        <td className="deposito">R$ 45.000.00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/06/2022</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </Container>
+    );
+}
