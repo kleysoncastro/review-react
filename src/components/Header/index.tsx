d113fef6d759fb8d2800ad64a78a994ee029c92b
kleyson Castro <kleysoncastro99@gmail.com>
2023-01-12 08:48:47 -0300

    chore: config modal base for page new transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
 import { Container, Content } from "./styles"
 import logoImg from "../../assets/logo.svg"
 export function Header() {
+    const [isNewOpenTransacitonModal, setIsNewOpenTransacitonModal] = useState(false);
+
+    function handleOpenNewTransaction() {
+        setIsNewOpenTransacitonModal(true);
+    }
+    function handleClaseNewTransaction() {
+        setIsNewOpenTransacitonModal(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="money" />
-                <button type="button">Nova Transação</button>
+                <button type="button"
+                    onClick={handleOpenNewTransaction}>Nova Transação</button>
+                <Modal isOpen={isNewOpenTransacitonModal} onRequestClose={handleClaseNewTransaction}>
+                    <h2>Nova transação</h2>
+                </Modal>
             </Content>
         </Container>
     )
