88fcd527fdb5d850b31de81d29c6a6864fd9f8d3
kleyson Castro <kleysoncastro99@gmail.com>
2023-01-12 09:38:33 -0300

    chore: add props component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import Modal from "react-modal";
+import React, { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './global';
 export function App() {
+
+  const [isNewOpenTransacitonModal, setIsNewOpenTransacitonModal] = useState(false);
+
+  function handleOpenNewTransaction() {
+    setIsNewOpenTransacitonModal(true);
+  }
+  function handleClaseNewTransaction() {
+    setIsNewOpenTransacitonModal(false);
+  }
+
   return (
+
     <>
-      <Header />
+      <Header props={handleOpenNewTransaction} />
       <Dashboard />
+      <Modal isOpen={isNewOpenTransacitonModal} onRequestClose={handleClaseNewTransaction}>
+        <h2>Nova transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
 import { Container, Content } from "./styles"
 import logoImg from "../../assets/logo.svg"
-export function Header() {
-    const [isNewOpenTransacitonModal, setIsNewOpenTransacitonModal] = useState(false);
 
-    function handleOpenNewTransaction() {
-        setIsNewOpenTransacitonModal(true);
-    }
-    function handleClaseNewTransaction() {
-        setIsNewOpenTransacitonModal(false);
-    }
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+export function Header(props: HeaderProps) {
+
 
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="money" />
                 <button type="button"
-                    onClick={handleOpenNewTransaction}>Nova Transação</button>
-                <Modal isOpen={isNewOpenTransacitonModal} onRequestClose={handleClaseNewTransaction}>
-                    <h2>Nova transação</h2>
-                </Modal>
+                    onClick={props.handleOpenNewTransaction}>Nova Transação</button>
+
             </Content>
         </Container>
     )
